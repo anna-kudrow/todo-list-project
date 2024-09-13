f052201d8a5dab14f5266616b72551353dc5e791
anna <anka-kudrow@mail.ru>
2024-09-13 14:37:28 +0300

    added edit fuuction

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,81 +1,87 @@
-import { ChangeEvent, useState } from "react";
-import Button from "../Button/Button";
-import "./Todo.css";
-import TodoItem from "../TodoItem/TodoItem";
-import { TodoItemType } from "../../types/TodoItem.type";
+import {ChangeEvent, useState} from 'react';
+import Button from '../Button/Button';
+import './Todo.css';
+import TodoItem from '../TodoItem/TodoItem';
+import {TodoItemType} from '../../types/TodoItem.type';
 
 function Todo() {
-  const [todoList, setTodoList] = useState<TodoItemType[]>([]);
+    const [todoList, setTodoList] = useState<TodoItemType[]>([]);
 
-  const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState('');
 
-  const [id, setId] = useState(1);
+    const [id, setId] = useState(1);
 
-  // const [isDone, setIsDone] = useState(false);
-
-  function createId() {
-    setId((id) => id + 1);
-  }
-
-  function updateInput(e: ChangeEvent<HTMLInputElement>): void {
-    setInputValue(e.target.value);
-  }
-
-  function addTodo(): void {
-    if (inputValue && inputValue.trim() !== "") {
-      createId();
-      const newItem: TodoItemType = { id: id, text: inputValue, done: false };
-      const newList = [...todoList, newItem];
-      setTodoList(newList);
-      setInputValue("");
+    function createId() {
+        setId(id => id + 1);
     }
-  }
 
-  function handleDelete(item: string): void {
-    setTodoList(todoList.filter((task) => task.text !== item));
-  }
+    function updateInput(e: ChangeEvent<HTMLInputElement>): void {
+        setInputValue(e.target.value);
+    }
 
-  function handleUpdateStatus(id: number): void {
-    const newList = todoList.map((item) => {
-      if (item.id === id) item.done = !item.done;
-      return item;
-    });
-    setTodoList(newList);
-  }
+    function addTodo(): void {
+        if (inputValue && inputValue.trim() !== '') {
+            createId();
+            const newItem: TodoItemType = {
+                id: id,
+                text: inputValue,
+                done: false,
+            };
+            const newList = [...todoList, newItem];
+            setTodoList(newList);
+            setInputValue('');
+        }
+    }
 
-  // function handleEdit () {
-  //     console.log('btn edit');
-  // }
+    function handleDelete(id: number): void {
+        setTodoList(todoList.filter(task => task.id !== id));
+    }
 
-  return (
-    <>
-      <h1 className="todo-title">Todo list</h1>
-      <div className="todo-box">
-        <div className="todo-field">
-          <input
-            type="text"
-            className="todo-input"
-            onChange={updateInput}
-            placeholder="add a task"
-            value={inputValue}
-          />
-          <Button onClick={addTodo}>ADD</Button>
-        </div>
-        <ul className="todo-list">
-          {todoList.map((item) => (
-            <TodoItem
-              id={item.id}
-              text={item.text}
-              isDone={item.done}
-              key={item.id}
-              onDelete={handleDelete}
-              onStatusChange={handleUpdateStatus}
-            />
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+    function handleUpdateStatus(id: number): void {
+        const newList = todoList.map(item => {
+            if (item.id === id) item.done = !item.done;
+            return item;
+        });
+        setTodoList(newList);
+    }
+
+    function handleEdit(id: number): void {
+        const editTask = todoList.find(task => task.id === id);
+        setInputValue(editTask.text);
+
+        setTodoList(todoList.filter(task => task.id !== id));
+    }
+
+    return (
+        <>
+            <h1 className="todo-title">Todo list</h1>
+            <div className="todo-box">
+                <div className="todo-field">
+                    <input
+                        type="text"
+                        className="todo-input"
+                        onChange={updateInput}
+                        placeholder="add a task"
+                        value={inputValue}
+                    />
+                    <Button onClick={addTodo}>ADD</Button>
+                </div>
+                <ul className="todo-list">
+                    {todoList.map(item => (
+                        <TodoItem
+                            id={item.id}
+                            text={item.text}
+                            isDone={item.done}
+                            key={item.id}
+                            onDelete={handleDelete}
+                            onStatusChange={handleUpdateStatus}
+                            onEdit={handleEdit}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default Todo;
